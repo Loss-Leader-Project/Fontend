7a26bdde83adc9420332ceb18b43c9469da037cd
Lee Min Wook <dnr14@naver.com>
2022-02-05 05:08:42 +0900

    feat : 프론트 초기 세팅 (#1)

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// const Test = styled.div`
+//   color: ${({ theme }) => {
+//     return theme.colors[0];
+//   }};
+//   ${({ theme }) => theme.paddingStyleGroup('10px')};
+// `;
+{
+  /* <img
+  src={`${process.env.PUBLIC_URL}/images/bookpie.png`}
+  alt='bookpie'
+/> */
+}
+const Main = () => {
+  return <div>메인페이지</div>;
+};
+
+export default Main;
