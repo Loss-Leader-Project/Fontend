14390058eec7022e75134c6a9fe0f4c3554a4023
junghwan kim <65111990+padd60@users.noreply.github.com>
2022-02-15 16:25:52 +0900

    Add : 아이디 및 비밀번호 찾기 페이지 UI 구현 및 Header 컴포넌트 라우터 기능 추가 + 테마 컬러 코드 변경 적용 (#10)

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -7,6 +7,8 @@ import SignUp from './pages/SignUp/SignUp';
 import Detail from './pages/Detail/Detail';
 import { Redirect } from 'react-router';
 import List from 'pages/List/List';
+import LoginSearchID from './pages/LogIn/LoginSearchID';
+import LoginSearchPW from './pages/LogIn/LoginSearchPW';
 import Header from 'Components/Header';
 
 const Routers = () => {
@@ -17,6 +19,8 @@ const Routers = () => {
         <Route exact path='/' component={Main} />
         <Route exact path='/my' component={My} />
         <Route exact path='/login' component={LogIn} />
+        <Route exact path='/login/searchID' component={LoginSearchID} />
+        <Route exact path='/login/searchPW' component={LoginSearchPW} />
         <Route exact path='/signup' component={SignUp} />
         <Route exact path='/product' component={Detail} />
         <Route exact path='/apply' component={Apply} />
