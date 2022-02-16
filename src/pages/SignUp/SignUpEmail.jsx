5fa2514385eab7caea8dc1f245906544e60994b4
junghwan kim <65111990+padd60@users.noreply.github.com>
2022-02-16 16:23:58 +0900

    Add : 회원가입 UI + 우편번호 카카오 라이브러리 적용 + 리뷰 반영 완료 (#13)

---
+++ src/pages/SignUp/SignUpEmail.jsx
@@ -0,0 +1,49 @@
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import React from 'react';
+
+const SignUpEmail = ({ email, flag, handleValue, mailHandleChange }) => {
+  return (
+    <>
+      {email === '직접입력' ? (
+        <TextField
+          name='email'
+          label='직접입력'
+          variant='outlined'
+          fullWidth
+          size='small'
+          InputLabelProps={{
+            style: { color: '#B9B9B9' },
+          }}
+          {...(flag.email ? { helperText: '메일 주소를 입력하세요', error: true } : {})}
+          onChange={handleValue}
+        />
+      ) : (
+        <FormControl fullWidth {...(flag.email ? { error: true } : {})}>
+          <InputLabel id='demo-simple-select-label' sx={{ marginTop: '-0.4375rem' }}>
+            주소를 선택하세요
+          </InputLabel>
+          <Select
+            name='email'
+            labelId='demo-simple-select-label'
+            id='demo-simple-select'
+            value={email}
+            size='small'
+            label='가입메일주소'
+            onChange={e => {
+              mailHandleChange(e);
+              handleValue(e);
+            }}
+          >
+            <MenuItem value={'직접입력'}>직접입력</MenuItem>
+            <MenuItem value={'naver.com'}>naver.com</MenuItem>
+            <MenuItem value={'google.com'}>google.com</MenuItem>
+            <MenuItem value={'daum.net'}>daum.net</MenuItem>
+          </Select>
+          {flag.email && <FormHelperText>메일주소를 선택해주세요</FormHelperText>}
+        </FormControl>
+      )}
+    </>
+  );
+};
+
+export default SignUpEmail;
---
+++ src/pages/SignUp/SignUpInput.jsx
@@ -0,0 +1,64 @@
+import { Grid, TextField } from '@mui/material';
+import React from 'react';
+import { faDotCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+
+const SignUpInput = props => {
+  return (
+    <CustomGridContainer>
+      <MustItem item lg={3}>
+        <ColorMustIcon {...(props.NotMust && { NotMust: true })}>
+          <FontAwesomeIcon icon={faDotCircle} size='xs' />
+        </ColorMustIcon>
+        <MustItemText>{props.itemText}</MustItemText>
+      </MustItem>
+      <Grid item lg={9}>
+        <TextField
+          name={props.name}
+          label={props.label}
+          variant='outlined'
+          fullWidth
+          type={props.password && 'password'}
+          autoComplete={props.autoComplete && 'current-password'}
+          size='small'
+          InputLabelProps={{
+            style: { color: '#B9B9B9' },
+          }}
+          {...(props.flag?.name ? { helperText: `${props.helperText}`, error: true } : {})}
+          onChange={props.handleValue}
+        />
+      </Grid>
+    </CustomGridContainer>
+  );
+};
+
+export default SignUpInput;
+
+const CustomGridContainer = styled(Grid).attrs(props => ({
+  container: true,
+  direction: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}))`
+  &&& {
+    margin-top: 1rem;
+  }
+  color: ${({ theme }) => theme.colors.gray};
+`;
+
+const MustItem = styled(Grid)`
+  display: flex;
+  flex-flow: row wrap;
+  opacity: ${({ NotText }) => (NotText ? 0 : 1)};
+`;
+
+const MustItemText = styled.div`
+  font-size: 1rem;
+  margin-left: 0.625rem;
+`;
+
+const ColorMustIcon = styled.span`
+  color: ${({ theme }) => theme.colors.brandColor};
+  opacity: ${({ NotMust }) => (NotMust ? 0 : 1)};
+`;
