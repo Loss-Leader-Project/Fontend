96c0ad5bdb9655897d9d1627902153d6c448e1e3
Lee Min Wook <dnr14@naver.com>
2022-03-15 18:30:41 +0900

    Fix : 공통 API 모듈 수정

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,9 +1,9 @@
 //Post랑 Get, Update 에 따라 같은 함수를 호출하는데 인자랑 url에 따라 다르게 동작함
 
 import axiosInstance from './apiInterceptors';
-import { HTTPError } from './api';
+import { HTTPError } from 'utils/httpErrorMessage';
 
-export const ApiRq = async (method, url, params, data) => {
+export const ApiRq = async (method, url, params = '', data) => {
   try {
     const res = await axiosInstance({
       method,
@@ -14,6 +14,6 @@ export const ApiRq = async (method, url, params, data) => {
     return res.data;
   } catch (error) {
     const message = HTTPError.errorHandler(error);
-    alert(message);
+    throw message;
   }
 };
